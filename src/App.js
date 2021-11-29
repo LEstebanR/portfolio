c1a5bf0019335c59a6a511f3e52f08f7b45dbfc3
Luis Esteban Ramírez <leramirezca@gmail.com>
2021-11-29 14:57:19 -0500

    remove parallax

--- src/App.js
+++ src/App.js
@@ -9,13 +9,11 @@ import Contact from './components/Contact.jsx';
 function App() {
   return (
     <div className="App">
-      <Parallax pages={5} style={{ top: '0', left: '0' }}>
-        <ParallaxLayer offset={0} speed={0.5}><Home /></ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0.5}><About /></ParallaxLayer>
-        <ParallaxLayer offset={2} speed={0.5}><Skills /></ParallaxLayer>
-        <ParallaxLayer offset={3} speed={0.5}><Projects /></ParallaxLayer>
-        <ParallaxLayer offset={4} speed={0.5}><Contact /></ParallaxLayer>
-      </Parallax>
+        <Home/>
+        <About/>
+        <Skills/>
+        <Projects/>
+        <Contact />
 
     </div>
   );
